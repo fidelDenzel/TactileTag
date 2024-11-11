b7803147c011b3bac92ae8c9a2f132f07ab2e7fe
fidelDenzel <fdenzelp@gmail.com>
2024-11-11 15:18:09 +0700

    amplified sound is working with DAC output to PAM8031 Amplifier

---
+++ test/Audio/dfplayer.cpp
@@ -0,0 +1,97 @@
+#include <Arduino.h>
+#include "DFRobotDFPlayerMini.h"
+
+DFRobotDFPlayerMini player; // Create the Player object
+
+void setup() {
+
+  Serial.begin(115200); // Init USB serial port for debugging
+
+  Serial2.begin(9600); // Init serial port for DFPlayer Mini
+
+  // Start communication with DFPlayer Mini
+
+  Serial.println("Connecting to DFplayer");
+
+  while (!player.begin(Serial2))
+
+  {
+
+      Serial.print(".");
+
+      delay(1000);
+
+  }
+
+  Serial.println(" DFplayer connected!");
+
+  player.volume(20); // Set volume to maximum (0 to 30).
+
+}
+
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+
+void loop() {
+    player.play(1);
+    Serial.println("Playing 1...");
+    delay(5000);
+
+    player.play(2);
+    Serial.println("Playing 2...");
+    delay(5000);
+
+   }
--- test/join1.cpp
+++ test/join1.cpp
@@ -1,12 +1,9 @@
 #include <Arduino.h>
-//  #include <WiFi.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <Wire.h>
 #include "DFRobotDFPlayerMini.h"
-// #include <driver/i2s.h>
-#include <math.h>
 
 #define buzzerPin 23
 #define TARGETDEV_1 "Slide"
@@ -14,9 +11,6 @@
 #define TARGETDEV_3 "SeaSaw"
 #define RSSI_TH -69
 #define RSSI_TOLERANCE 5
-// #define I2S_DOUT 25
-// #define I2S_BCK 27
-// #define I2S_WS 26
 
 DFRobotDFPlayerMini player; // Create the Player object
 
@@ -26,13 +20,6 @@ BLEScan *pBLEScan;
 // Variables to track the closest filtered device
 String closestDeviceName = "";
 int closestRSSI = -999;
-
-// Buffer to hold sine wave samples
-int16_t samples[100];
-
-const int sampleRate = 44100;    // Sample rate in Hz
-const int amplitude = 3000;      // Amplitude of the sine wave
-const int frequency = 1000;      // Frequency of the sine wave in Hz (1 kHz)
 
 void setup()
 {
@@ -56,39 +43,6 @@ void setup()
 
   // Set buzzer pin as output
   pinMode(buzzerPin, OUTPUT);
-
-  // // Configure I2S for output
-  // i2s_config_t i2s_config = {
-  //   .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-  //   .sample_rate = sampleRate,
-  //   .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-  //   .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-  //   .communication_format = I2S_COMM_FORMAT_STAND_MSB,
-  //   .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-  //   .dma_buf_count = 8,
-  //   .dma_buf_len = 64,
-  //   .use_apll = false
-  // };
-
-  // i2s_pin_config_t pin_config = {
-  //   .bck_io_num = I2S_BCK,
-  //   .ws_io_num = I2S_WS,
-  //   .data_out_num = I2S_DOUT,
-  //   .data_in_num = I2S_PIN_NO_CHANGE
-  // };
-
-  // // Install and start the I2S driver
-  // i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-  // i2s_set_pin(I2S_NUM_0, &pin_config);
-  // i2s_set_clk(I2S_NUM_0, sampleRate, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_STEREO);
-
-  // // Generate a sine wave in the buffer
-  // for (int i = 0; i < 100; i++) {
-  //   samples[i] = amplitude * sin(2 * M_PI * frequency * i / sampleRate);
-  // }
-
-  // Serial.println("I2S initialized - Outputting 1kHz sine wave");
-
 }
 
 // Function to check if a device name matches any of the target names
@@ -110,7 +64,6 @@ bool isTargetDevice(String deviceName)
 
 void loop()
 {
-  size_t bytesWritten;
   // Serial.println("Scanning for BLE devices...");
 
   BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
@@ -160,21 +113,18 @@ void loop()
     {
       toneFrequency = 1000;
       player.play(1);
-      // i2s_write(I2S_NUM_0, samples, sizeof(samples), &bytesWritten, portMAX_DELAY);
       delay(1000);
     }
     else if ((closestDeviceName == "Swing"))
     {
       toneFrequency = 5000;
       player.play(2);
-      // i2s_write(I2S_NUM_0, samples, sizeof(samples), &bytesWritten, portMAX_DELAY);
       delay(1000);
     }
     Serial.printf("Freq = %d\n", toneFrequency);
     tone(buzzerPin, toneFrequency, 500); // Play tone for 500ms
-    
-    // Wait before the next scan
-    delay(500);
+  // Wait before the next scan
+  delay(500);
   }
   else
   {
@@ -183,7 +133,6 @@ void loop()
     // Serial.println("No filtered devices found.");
     Serial.println("Please enter the playground!");
     player.play(3);
-    // i2s_write(I2S_NUM_0, samples, sizeof(samples), &bytesWritten, portMAX_DELAY);
     delay(3000);
     // tone(buzzerPin, 100, 500);
     // tone(buzzerPin, 10000, 500);
