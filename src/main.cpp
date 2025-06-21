07d093e1443552af6bd9bdb6d8015748cc190c72
fidelDenzel <fdenzelp@gmail.com>
2025-06-21 13:25:03 +0700

    2025-06-21, commenting espnow on the server end or beacon

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #define SLIDE_IDX 1
 #define SWING_IDX 2
 
-#define WHICH_BEACON SWING_IDX
+#define WHICH_BEACON SLIDE_IDX
 #define TARGETDEV_1 "ESP32-Perosotan"
 #define TARGETDEV_2 "ESP32-Ayunan"
 // #define TARGETDEV_3 "SeaSaw"
@@ -16,23 +16,23 @@ DFRobotDFPlayerMini player; // Create the Player object
 
 const std::__cxx11::string targetDeviceNames[] = {TARGETDEV_1, TARGETDEV_2};
 
-// MAC's Beacon : A0:DD:6C:AF:6C:64
-typedef struct scanner_command
-{
-    int scanner_id;
-    bool soundStatus;
-} scanner_command;
+// // MAC's Beacon : A0:DD:6C:AF:6C:64
+// typedef struct scanner_command
+// {
+//     int scanner_id;
+//     bool soundStatus;
+// } scanner_command;
 
-scanner_command scannerCommand;
+// scanner_command scannerCommand;
 
 long nowTime = 0;
 
-void onReceive(const uint8_t *macAddr, const uint8_t *incomingData, int len)
-{
-    nowTime = millis();
-    memcpy(&scannerCommand, incomingData, sizeof(scannerCommand));
-    Serial.printf("Sound status = %d\n", scannerCommand.soundStatus);
-}
+// void onReceive(const uint8_t *macAddr, const uint8_t *incomingData, int len)
+// {
+//     nowTime = millis();
+//     memcpy(&scannerCommand, incomingData, sizeof(scannerCommand));
+//     Serial.printf("Sound status = %d\n", scannerCommand.soundStatus);
+// }
 
 // Callback to notify connection events
 class MyServerCallbacks : public NimBLEServerCallbacks {
@@ -80,30 +80,30 @@ void setupDFP(int dfpVolume)
     player.volume(dfpVolume); // Set volume to maximum (0 to 30).
 }
 
-// Structure example to receive data
-// Must match the sender structure
-typedef struct test_struct
-{
-    int id;
-    bool soundStatus;
-} test_struct;
+// // Structure example to receive data
+// // Must match the sender structure
+// typedef struct test_struct
+// {
+//     int id;
+//     bool soundStatus;
+// } test_struct;
 
-// Create a struct_message called myData
-test_struct myData;
+// // Create a struct_message called myData
+// test_struct myData;
 
-// callback function that will be executed when data is received
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
-{
-    nowTime = millis();
-    memcpy(&myData, incomingData, sizeof(myData));
-    Serial.print("Bytes received: ");
-    Serial.println(len);
-    Serial.print("x: ");
-    Serial.println(myData.id);
-    Serial.print("sound status: ");
-    Serial.println(myData.soundStatus);
-    Serial.println();
-}
+// // callback function that will be executed when data is received
+// void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+// {
+//     nowTime = millis();
+//     memcpy(&myData, incomingData, sizeof(myData));
+//     Serial.print("Bytes received: ");
+//     Serial.println(len);
+//     Serial.print("x: ");
+//     Serial.println(myData.id);
+//     Serial.print("sound status: ");
+//     Serial.println(myData.soundStatus);
+//     Serial.println();
+// }
 
 void setup()
 {
@@ -116,30 +116,30 @@ void setup()
 
     setupNimBLEServer();
 
-    // Set device as a Wi-Fi Station
-    WiFi.mode(WIFI_STA);
+    // // Set device as a Wi-Fi Station
+    // WiFi.mode(WIFI_STA);
 
-    // Init ESP-NOW
-    if (esp_now_init() != ESP_OK)
-    {
-        Serial.println("Error initializing ESP-NOW");
-        return;
-    }
+    // // Init ESP-NOW
+    // if (esp_now_init() != ESP_OK)
+    // {
+    //     Serial.println("Error initializing ESP-NOW");
+    //     return;
+    // }
 
-    // Once ESPNow is successfully Init, we will register for recv CB to
-    // get recv packer info
-    esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+//     // Once ESPNow is successfully Init, we will register for recv CB to
+//     // get recv packer info
+//     esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
 }
 
 void loop()
 {
     // turn sound at beacon off after 5 seconds
-    if(millis() - nowTime >= PLAYSOUND_DELAY && myData.soundStatus){
+    if(millis() - nowTime >= PLAYSOUND_DELAY){ // && myData.soundStatus){
         Serial.println("Turning off sound");
-        myData.soundStatus = 0;
+        // myData.soundStatus = 0;
     }
-    if(myData.soundStatus){
-        // player.play(WHICH_BEACON);
-        // delay(2000);
-    }
+    // if(myData.soundStatus){
+    //     // player.play(WHICH_BEACON);
+    //     // delay(2000);
+    // }
 }
